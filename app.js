2388a0aa34a8c6e6b4c6a442e5ec6cf5fd1a3fbb
Grosul Igor <grosuligor99@mail.ru>
2023-01-05 15:05:32 +0200

    3

--- app.js
+++ app.js
@@ -17,18 +17,46 @@ squareOne.addEventListener("click", function(){
     squareOne.textContent = "X";
     squareOne.style.fontSize = "110px";
     squareOne.style.textAlign = "center";
-    
 });
 
 
 squareOne.addEventListener("dblclick", function() {
-    if (squareOne.clicked == true) {
-
-    } else {
+    if (!squareOne.clicked == true) {
         squareOne.textContent = "O";
         squareOne.style.fontSize = "110px";
         squareOne.style.textAlign = "center";
-
     }
+});
 
-})
+
+squareTwo.addEventListener("click", function(){
+    squareTwo.textContent = "X";
+    squareTwo.style.fontSize = "110px";
+    squareTwo.style.textAlign = "center";
+});
+
+
+squareTwo.addEventListener("dblclick", function() {
+    if (!squareTwo.clicked == true) {
+        squareTwo.textContent = "O";
+        squareTwo.style.fontSize = "110px";
+        squareTwo.style.textAlign = "center";
+    }
+});
+
+
+
+squareThree.addEventListener("click", function(){
+    squareThree.textContent = "X";
+    squareThree.style.fontSize = "110px";
+    squareThree.style.textAlign = "center";
+});
+
+
+squareThree.addEventListener("dblclick", function() {
+    if (!squareThree.clicked == true) {
+        squareThree.textContent = "O";
+        squareThree.style.fontSize = "110px";
+        squareThree.style.textAlign = "center";
+    }
+});
